a0363f1dbf014698c7d4cd66b12cf5ad81674a67
Muhammad Yoga Nugraha Putra <m.yoga@agate.id>
2020-01-22 14:43:08 +0700

    set rate spawn and falling speed food

--- project/catch-apple/src/js/subcontroller/good_food.js
+++ project/catch-apple/src/js/subcontroller/good_food.js
@@ -13,6 +13,7 @@ export default class GoodFood {
 
     InitData(){
         this.goodStuff = [ 'good', 'good2', 'good3', 'good4', 'good5'];
+        this.fallSpeed = 500;
     }
 
     SpawnFood(x, y){
@@ -20,7 +21,7 @@ export default class GoodFood {
         this.fruit = this.scene.physics.add.image(x, y, this.goodStuff[this.rand]);
         this.fruit.displayWidth = this.ScreenUtility.GameWidth * 0.15;
         this.fruit.displayHeight = this.fruit.displayWidth;
-        this.SetFallingSpeed(500);
+        this.SetFallingSpeed(this.fallSpeed);
     }
 
     RandomTexture(){
