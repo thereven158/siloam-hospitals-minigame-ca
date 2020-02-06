f3e6191f29e1dde4040a8c1981afa958b215dc89
Muhammad Yoga Nugraha Putra <m.yoga@agate.id>
2020-02-06 09:25:38 +0700

    loading bar stuck issue 99%

--- project/catch-apple/src/js/scene/loading/subloader/loading_gameplay_controller.js
+++ project/catch-apple/src/js/scene/loading/subloader/loading_gameplay_controller.js
@@ -38,9 +38,5 @@ export default class LoadingGameplayController {
       this.scene.load.image('window-top',this.scene.CreatePath('/images/env/ingame_env_cloud_main.png'));
       this.scene.load.image('background_gameplay',this.scene.CreatePath('/images/env/ingame_still_backgound.png'));
       this.scene.load.image('trans_topcloud',this.scene.CreatePath('/images/env/ingame_env_cloud_second.png'));
-      this.scene.load.video('cusotm', this.scene.CreatePath('/video/padoru.mp4'));
-      // this.scene.load.video('cusotm', this.scene.CreatePath("https://storage.googleapis.com/bucket-storage-siloam-hospital.gf.agatedev.net/23b57541562146e5b64a688d6db43134.mp4?GoogleAccessId=siloam-hospital-sa@agate-gamification.iam.gserviceaccount.com&Expires=1580895285&Signature=R7Ip2X%2B2359F4kguUv8dsjTUhpR1z7OJauWMQj%2Bs5kj7JytY00lkWFyJ2ekQM%2F1TgdqST7LM7r1ISNVt9Nfgz11ZWh6lSWLDrlxk1hxyeyb5oKzQBprNknf3fF9yiWQk2jIrs0ElsjAKJjtNgKURGNlVp3gKDgGe11uWhdEa3fmcKRl%2Be5L4Jp%2BJ9IkdWJzv19pv7eVXapXxk0hY78sOEyhlpz2UFmQy14Iv2yw18yy3uNkxLRfKAICJg9N2uCXklGQlcYXYNzAzlyzXakAhLVhrJ5iKqDEz3I%2FYakXH0iE7Xv1tpSxQX72SREr9bsUT%2FErj4vK%2F0Eu4NjIYQvNWXw%3D%3D"));
-      
-      this.scene.load.image('cusotm2', this.scene.CreatePath('/video/catree.png'));
      }
 }
